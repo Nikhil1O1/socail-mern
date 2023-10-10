ff0c4161e463be6d982ed2da8fc34f078c75ea98
Nikhil1O1 <technikue2.0@gmail.com>
2023-10-11 01:25:07 +0530

    implemetned post editing
    when clicking on edit post, the post data copies to form and then can be edited
    
    KNOWN ERROR = Page doesn't auto reload after submitting data

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,7 @@
 export default (posts = [], action) => { // reducers state are supposed to be a value so we send an empty array
     switch (action.type) {
+        case 'UPDATE':
+            return posts.map((post)=> post._id === action.payload._id ? action.payload : post) 
         case 'FETCH_ALL':
             return action.payload ;
         case 'CREATE':
