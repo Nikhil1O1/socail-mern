2c8b5cd6577adbf3d4c33d9c73fbb22f39f0cb10
Nikhil1O1 <technikue2.0@gmail.com>
2023-10-29 00:37:25 +0530

    at 2:37:00, added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import {useDispatch} from 'react-redux';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 const App = () => {
   const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
@@ -18,13 +18,10 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className = {classes.heading} variant='h2' align='center'>Memories</Typography>
-        <img className = {classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
+      <Navbar/>
       <Grow in>
         <Container>
-          <Grid className={classes.mainCOntainer} container  justify='space-between' alignItems='stretch' spacing={3}>
+          <Grid className={classes.mainContainer} container  justify='space-between' alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}/>
             </Grid>
